e2db6778ef87c6a7589b4c86aad1b995f192e070
CristianSR4 <cristian.lerida.sanchez@gmail.com>
2018-03-12 20:24:42 +0100

    Test de Errores

--- CalculadoraTest.java
+++ CalculadoraTest.java
@@ -8,42 +8,104 @@ import org.junit.Test;
 /**
  * The test class CalculadoraTest.
  *
- * @author  (your name)
+ * @author  (Daniel Villalobos y Cristian Lerida)
  * @version (a version number or a date)
  */
 public class CalculadoraTest
 {
-    private Operacion[] operacio1;
-    private Operacion sUMA1;
-    private Operacion pOTENCIA1;
+    private double num1;
+    private double num2;
+    private double rdo;
+    private Operacion op;
 
     /**
      * Default constructor for test class CalculadoraTest
      */
     public CalculadoraTest()
     {
+        num1=0.0;
+        num2=0.0;
+        op=op.SUMA;
     }
 
-    /**
-     * Sets up the test fixture.
-     *
-     * Called before every test case method.
-     */
-    @Before
-    public void setUp()
-    {
-        operacio1 = Operacion.values();
-        sUMA1 = operacio1[0];
-        pOTENCIA1 = operacio1[4];
+    @Test
+    public void testSuma() {
+        Calculadora calc = new Calculadora();
+         calc.ponNum1(2);
+         calc.ponNum2(4);
+         calc.ponOperacion("SUMA");
+         calc.opera();
+         assertEquals(6 , calc.dameResultado(),0.2);
     }
-
-    /**
-     * Tears down the test fixture.
-     *
-     * Called after every test case method.
-     */
-    @After
-    public void tearDown()
-    {
+    
+    @Test
+    public void testResta() { 
+        Calculadora calc = new Calculadora();
+         calc.ponNum1(4);
+         calc.ponNum2(2);
+         calc.ponOperacion("RESTA");
+         calc.opera();
+         assertEquals(2 , calc.dameResultado(),0.2);
+     }
+     
+    @Test
+    public void   testMultiplicacion() { 
+        Calculadora calc = new Calculadora();
+         calc.ponNum1(5);
+         calc.ponNum2(5);
+         calc.ponOperacion("MULTIPLICA");
+         calc.opera();
+         assertEquals(25 , calc.dameResultado(),0.2);
+ }
+ 
+    @Test
+    public void   testDivision() { 
+        Calculadora calc = new Calculadora();
+         calc.ponNum1(10);
+         calc.ponNum2(2);
+         calc.ponOperacion("DIVIDE");
+         calc.opera();
+         assertEquals( 5, calc.dameResultado(),0.2);      
+    } 
+    
+     @Test
+    public void   testPotencia1() { 
+        Calculadora calc = new Calculadora();
+         calc.ponNum1(2);
+         calc.ponNum2(10);
+         calc.ponOperacion("POTENCIA");
+         calc.opera();
+         assertEquals(1024 ,calc.dameResultado(),0.2);        
     }
+    
+     @Test
+    public void testRAIZ() { 
+        Calculadora calc = new Calculadora();
+         calc.ponNum1(5);
+         calc.ponNum2(10);
+         calc.ponOperacion("RAIZ");
+         calc.opera();
+         assertEquals(1.17 , calc.dameResultado(),0.2);     
+    }
+    
+    @Test
+    public void testLogaritmo() { 
+        Calculadora calc = new Calculadora();
+         calc.ponNum1(5);
+         calc.ponNum2(10);
+         calc.ponOperacion("LOGARITMO");
+         calc.opera();
+         assertEquals(2.61 , calc.dameResultado(),0.2);     
+    }
+    
+     @Test
+    public void testFactoriales() { 
+        Calculadora calc = new Calculadora();
+         calc.ponNum1(5);
+         calc.ponNum2(10);
+         calc.ponOperacion("FACTORIALES");
+         calc.opera();
+         assertEquals(0.0 , calc.dameResultado(),0.2);     
+    }
+  
 }
